91f053d710f85175126d032db7243c2e8808ea8d
vsunholy <vitalijsnopov@gmail.com>
2025-03-13 09:00:45 +0200

    enhance To Do James tests by adding item deletion and update functionality

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -16,7 +16,7 @@ test.describe('To Do James Tests', () => {
     await page.goto('https://todolist.james.am/#/');
 
     await expect(page).toHaveTitle(/To Do List/);
-    
+
   })
 
   test('add new item', async ({ page }) => {
@@ -29,15 +29,48 @@ test.describe('To Do James Tests', () => {
     await expect(page.getByText('1 uzduotis')).toBeVisible();
   })
   test('delete item', async ({ page }) => {
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    const todoItem = page.locator('ul.todo-list li:has-text("1 uzduotis")');
+    await todoItem.hover();
+    await todoItem.locator('button.destroy').click();
+    await expect(todoItem).toHaveCount(0);
+  });
+
+  test('update item', async ({ page }) => {
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+   
+    const todoItem = page.locator('ul.todo-list li', { hasText: '1 uzduotis' });
+    await todoItem.dblclick();
+
+    const editInput = page.locator('input.edit');
+    await editInput.fill('paredagavom');
+    await editInput.press('Enter');
+
+    const editedItem = page.locator('ul.todo-list li', { hasText: 'paredagavom' });
+    await expect(editedItem).toBeVisible();
+
+   
+    // await page.locator('ul.todo-list li', { hasText: '1 uzduotis' }).dblclick();
 
 
-  })
-  test('update', async ({ page }) => {
+});
 
-
-  })
   test("count", async ({ page }) => {
-    
-    
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    await page.fill('input.new-todo', '1 uzduotis');
+    await page.press('input.new-todo', 'Enter');
+    const fullList = page.locator('ul.todo-list li');
+    await expect(fullList).toHaveCount(6);
   });
 })
